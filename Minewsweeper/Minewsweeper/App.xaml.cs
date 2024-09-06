26662b27368e2829b962e0acdd156f2834ea022e
Alex McRae <mcrae.alex@hotmail.com>
2024-09-06 17:30:40 -0400

    Created initial solution

---
+++ Minewsweeper/Minewsweeper/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Minewsweeper
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
